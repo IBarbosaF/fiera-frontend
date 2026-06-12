c1b3a83c960358b6bfc365861c9b5f11a47d1e57
IBarbosaF <ibarbosaferrer91@gmail.com>
2026-06-12 21:02:19 +0200

    Auto-generated commit

---
+++ chunk-CFBXHHGJ.js
@@ -0,0 +1,1 @@
+import{Qa as a,Ra as n,cb as o,ya as l}from"./chunk-Z2BDYI2P.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=l({type:t,selectors:[["app-club-detalle"]],decls:2,vars:0,template:function(e,i){e&1&&(a(0,"p"),o(1,"club-detalle works!"),n())},encapsulation:2})};export{s as ClubDetalle};
